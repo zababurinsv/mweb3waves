d32d546e3b4dd2566af77a5dac8e832e6619225b
Sergey <s.zabbaurin.v@gmail.com>
2019-05-23 21:03:23 +0300

    init

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const app = express();
+app.use(express.static(__dirname + '/dist'))
 
 app.get('/', (req, res) => {
     res.sendFile('index.html', { root : __dirname});
